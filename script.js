2c7c192d9d57483f1ec8099f9e894450f44d3095
kosmos434 <kosmos434@protonmail.com>
2019-09-28 18:36:52 -0500

    add newGrid() function

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function addDivs() {
 }
 
 
+function newGrid() {
+    let input = prompt("What width you want?");
+    let size = parseInt(input);
+    document.getElementById('container').innerHTML = "";
+    let divTotal = size * size
+    for (let i = 0; i < divTotal; i++) {
+        let littleDiv = document.createElement('div');
+        littleDiv.setAttribute("class", "littleDiv");
+        littleDiv.addEventListener("mouseenter", event => event.target.style.backgroundColor = "black")
+        document.getElementById('container').appendChild(littleDiv);
+    }
+    document.getElementById('container').style.gridTemplateColumns = 'repeat(' + size + ', 1fr)';
+    document.getElementById('container').style.gridTemplateRows = 'repeat(' + size + ', 1fr)';
+}
 
 
 addDivs();
